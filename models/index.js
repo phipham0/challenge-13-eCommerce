bfc6827d6cdb989ea58f5b1ed2b81430487d7a38
phipham0 <90461213+phipham0@users.noreply.github.com>
2023-11-12 21:38:19 -0600

    Finished model scripts in models folder

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
+// Categories have many Products
+Categeory.HasMany(Product, {
+  foreignKey: 'category_id',
+})
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unqiue: false
+  },
+  as: 'product_tag'
+})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tag_product'
+})
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
